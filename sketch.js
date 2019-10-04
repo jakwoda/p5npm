c87255dd0e6ce1a2078974c544be3a04f1e6ff33
Mateusz  Mieciek <Jak@Mateuszs-MacBook-Pro.local>
2019-10-04 02:15:44 +0200

    initial commit

---
+++ sketch.js
@@ -0,0 +1,14 @@
+let w = document.documentElement.clientWidth;
+let h = document.documentElement.clientHeight;
+function setup() {
+  createCanvas(w, 1000);
+}
+function draw() {
+  if (mouseIsPressed) {
+    ellipse(mouseX, mouseY, 50, 50);
+  }
+}
+
+function doubleClicked() {
+  background(255);
+}
